32ff9b51c6ad192ab2618a04817cf71ba62c8f77
maaxlucky <kenyangn00@gmail.com>
2024-04-30 18:49:51 +0300

    Water container

--- python/problems/medium/15-container-water.py
+++ python/problems/medium/15-container-water.py
@@ -14,24 +14,43 @@ from functools import reduce
 # Output: 49
 # Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 
-# Works, but very slow(Time limit exceeded)
-# Future idea of Algorithm
-# calculate container beetwen i and j, and from i to the start, in theory it can be in done in O(n)!
-# but this is for today
+
+# Algorithm : array [2,3,4,5,18,17,6]
+# We can easily calculate 'area of rectangle' by this formula and using 2 pointers: 
+# 1) Declare 2 pointers, i = 0 and j = len(arr) - 1 and Move pointers until i < j
+# 2) Calculate 'step' or 'vertex' of rectangle by comparing array[i] with array[j]
+# 3) After comparing, find the smallest one -> this is vertex, and find area of this rectangle
+# 4) Area is 'vertex' * length of rectangle => 'vertex' * (j - i)
+# 5) Which pointer to move? If array[i] is less than array[j] move pointer i forward, otherwise move 'j' pointer
+# 6) For each [i:j] pair we calculate MAXIMUM area at this moment (their snapshot) for example for our array, we have
+# [2.....6] area is 1) minimum of array[0] and array[6] = 2(vertex), length is '6(j) - 0(i)' = 0(length) => 2 * 6 = 12
+# area is 12, and AREA CAN'T BE MORE AND WON'T BE MORE ANYMORE(for example we move last pointer, now length is 5, and
+# area is 10 and so on, there is no point to check area for these pointers, so we should move forward),
+# that's why we move smallest pointer, we want to find area with bigger vertex.
+
 class Solution(object):
     def maxArea(self, height):
-        result = 0
-        for i in range(len(height)):
-            for j in range(i + 1, len(height)):
-                if height[i] > height[j]:
-                    step = height[j]
-                else:
-                    step = height[i]
-                sumContainer = step * (j-i)
-                if sumContainer > result:
-                    result = sumContainer
+        i = 0
+        j = len(height) - 1
+        tmpScore = 0
+        score = 0
+        while i < j:
+            step = 0
+            if height[i] < height[j]:
+                step = height[i]
+            else:
+                step = height[j]
+            tmpLength = j - i
+            tmpScore = tmpLength * step
+            print(tmpScore)
+            if tmpScore > score:
+                score = tmpScore
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
 
-        return result
+        return score
 
 
 sol = Solution()
