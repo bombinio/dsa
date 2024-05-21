4bcf7dbd1853a0a73928dce487d21dd0333050b2
maaxkk <kenyangn00@gmail.com>
2024-05-21 20:19:30 +0300

    revisit problem that could not solve

--- javascript/problems/medium/27-find-minimum.js
+++ javascript/problems/medium/27-find-minimum.js
@@ -18,13 +18,29 @@
 // Explanation: The original array was [1,2,3,4,5] rotated 3 times.
 
 // Algos
-// 1) Check first index of array, if it's less than 0+1 and arr.length - 1, it means it's lowest number
-// 2) Otherwise i = arr.len - 1, now check arr.len - 1 - 1, if it's bigger than i, then i is the lowest number
-// 3) Otherwise we search in range [1:arr.len-1-1] like in ordinary binary search
+// 1) If our mid number is less then right number, it means we are in sequence and we should move in left direction
+// 2) We move our right pointer to the middle-1 like in ordinary binary search, and not to lost current mid
+// we compare it with 'currMin' and if first mid number was actually the lowest, we already saved it
+// 3) Then we just repeat this 'algo' until left <= right, e.g [5,1], left is 0, mid is 0, right is 1, arr[mid] < arr[right]
+// we move left pointer, left = 1, mid = 1 right = 1 => we searched all numbers, time to left from cycle, in the next
+// iteration loop will break;
 
 // TODO
 const findMin = function(nums) {
-
+    let left = 0
+    let right = nums.length - 1;
+    let currMin = nums[0];
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        currMin = Math.min(currMin, nums[mid])
+        if (nums[mid] < nums[right]) { // it means we have sequence in range [mid:right] and we must go only to the left
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return currMin;
 }
 
 
